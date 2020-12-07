5b4bb307c859ac00c72270fcbc64e0e736e1362e
Daniel Meszaros <easimer@gmail.com>
2020-12-07 07:24:18 +0100

    Day 7, part 2

--- day7/day7.nim
+++ day7/day7.nim
@@ -14,13 +14,13 @@ type
 func parseLine(line: string): BagRule =
   let splitContains = split(line, " bags contain ")
   discard scanf(splitContains[0], "$w $w", result.lhs.kind, result.lhs.color)
-  let rhss = split(splitContains[1], ", ")
-  for rhs in rhss:
-    var b: Bag
-    var n: int
-    discard scanf(rhs, "$i $w $w", n, b.kind, b.color)
-    result.rhs[b] = n
-  debugEcho(result)
+  if splitContains[1] != "no other bags.":
+    let rhss = split(splitContains[1], ", ")
+    for rhs in rhss:
+      var b: Bag
+      var n: int
+      discard scanf(rhs, "$i $w $w", n, b.kind, b.color)
+      result.rhs[b] = n
 
 func part1(rules: seq[BagRule]): string =
   let myBag: Bag = (kind: "shiny", color: "gold")
@@ -41,6 +41,20 @@ func part1(rules: seq[BagRule]): string =
 
   return $len(mayContain)
 
+func howManyBagsAreRequiredInside(ruleTable: Table[Bag, Table[Bag, int]], bag: Bag): int =
+  result = 1
+
+  for k,v in ruleTable[bag]:
+    result += v * ruleTable.howManyBagsAreRequiredInside(k)
+
+func part2(rules: seq[BagRule]): string =
+  let ruleTable = rules.toTable()
+  let myBag: Bag = (kind: "shiny", color: "gold")
+
+  # Don't count the shiny gold bag itself
+  return $(ruleTable.howManyBagsAreRequiredInside(myBag) - 1)
+
+
 when isMainModule:
   var
     f: File
@@ -54,8 +68,7 @@ when isMainModule:
       rules.add(rule)
 
     let res1 = part1(rules)
-    #let res2 = countElements(groupsIntersected)
-    let res2 = ""
+    let res2 = part2(rules)
 
     echo(%*{"output1": res1, "output2": res2})
   else:
