8ef7aa212634514e8c6265332567d875c27c54c3
Daniel Meszaros <easimer@gmail.com>
2020-12-14 06:31:40 +0100

    Day 14, p2

--- day14/day14.nim
+++ day14/day14.nim
@@ -20,7 +20,7 @@ type
     of kMask:
       mask: string
     of kWrite:
-      address: int
+      address: string
       value: string
 
 func parseMask(s: string): InputLine =
@@ -31,8 +31,10 @@ func parseMask(s: string): InputLine =
 func parseWrite(s: string): InputLine =
   result = InputLine(kind: kWrite)
   var value: int
-  if not scanf(s, "mem[$i] = $i", result.address, value):
+  var address: int
+  if not scanf(s, "mem[$i] = $i", address, value):
     quit(1)
+  result.address = toBin(address, 36)
   result.value = toBin(value, 36)
 
 func applyMask(value: string, mask: string): string =
@@ -43,6 +45,28 @@ func applyMask(value: string, mask: string): string =
     else:
       result[i] = mask[i]
 
+proc broadcastWrite(mem: var Table[int, string], address: string, value: string, i: int) =
+  if i != 36:
+    if address[i] == 'X':
+      var copy0 = address
+      copy0[i] = '0'
+      broadcastWrite(mem, copy0, value, i + 1)
+      copy0[i] = '1'
+      broadcastWrite(mem, copy0, value, i + 1)
+    else:
+      broadcastWrite(mem, address, value, i + 1)
+  else:
+    mem[parseBinInt(address)] = value
+
+proc broadcastWrite(mem: var Table[int, string], address: string, value: string, mask: string) =
+  var copy0 = address
+  for i in 0 .. high(address):
+    if mask[i] != '0':
+      copy0[i] = mask[i]
+
+  broadcastWrite(mem, copy0, value, 0)
+
+
 func part1(input: seq[InputLine]): string =
   var memory = initTable[int, string]()
   var mask = ""
@@ -52,7 +76,24 @@ func part1(input: seq[InputLine]): string =
       of kMask:
         mask = i.mask
       of kWrite:
-        memory[i.address] = applyMask(i.value, mask)
+        memory[parseBinInt(i.address)] = applyMask(i.value, mask)
+
+  var sum = 0
+  for k,v in memory:
+    sum += parseBinInt(v)
+
+  return $sum
+
+func part2(input: seq[InputLine]): string =
+  var memory = initTable[int, string]()
+  var mask = ""
+
+  for i in input:
+    case i.kind:
+      of kMask:
+        mask = i.mask
+      of kWrite:
+        broadcastWrite(memory, i.address, i.value, mask)
 
   var sum = 0
   for k,v in memory:
@@ -76,8 +117,7 @@ when isMainModule:
           parseWrite(line)
 
     let res1 = part1(input)
-    #let res2 = part2(notes)
-    let res2 = ""
+    let res2 = part2(input)
 
     echo(%*{"output1": res1, "output2": res2})
   else:
